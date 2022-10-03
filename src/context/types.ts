21fb6d74044193bd6e7687ff9e04cad212835472
manosliod <manosliod@gmail.com>
2022-10-03 19:40:15 -0400

    commit storex project with login auth semi-done

---
+++ src/context/types.ts
@@ -0,0 +1,38 @@
+export type ErrCallbackType = (err: { [key: string]: string }) => void
+
+export type LoginParams = {
+  email: string
+  password: string
+}
+
+export type RegisterParams = {
+  email: string
+  username: string
+  password: string
+}
+
+export type UserDataType = {
+  _id?: string | null
+  id: UserDataType['_id']
+  name?: string | null
+  fullName: UserDataType['name']
+  role: string
+  email: string
+  password: string
+  phone?: string | null
+  birthday?: string | null
+  avatar?: string | null
+}
+
+export type AuthValuesType = {
+  loading: boolean
+  setLoading: (value: boolean) => void
+  logout: () => void
+  isInitialized: boolean
+  user: UserDataType | null
+  setUser: (value: UserDataType | null) => void
+  setIsInitialized: (value: boolean) => void
+  login: (params: LoginParams, errorCallback?: ErrCallbackType) => void
+  register: (params: RegisterParams, errorCallback?: ErrCallbackType) => void,
+  setUsers: (data: UserDataType[]) => void
+}
